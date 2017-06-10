22fbba7da2ee07d6d3830e31c9bbe326633bc261
Liroo <pierre-monge@hotmail.fr>
2017-06-10 07:17:22 +0200

    👴  Remove old code from fake iteration

--- irc_server/include/event.h
+++ irc_server/include/event.h
@@ -5,7 +5,7 @@
 ** Login   <pierre@epitech.net>
 **
 ** Started on  Tue May 30 06:28:44 2017 Pierre Monge
-** Last update Sat Jun 10 04:57:08 2017 Pierre Monge
+** Last update Sat Jun 10 07:16:24 2017 Pierre Monge
 */
 
 #ifndef EVENT_H
@@ -16,17 +16,6 @@
 # define EVENT char
 # define EVENT_READ 0x1
 # define EVENT_WRITE 0x2
-
-/*
-** This macro is not lovely, I know
-** This is a fake for loop using iteration + continue
-** to re-invent the for/goto looping
-**
-** I did not like it but I have to.
-** The other way was to split the event_dispatch loop
-** in two function and this is horible to read...
-*/
-# define FOR_ITERATION(it) it = it + 1; continue;
 
 /*
 ** event_dispatch - dispatch event returned from select
